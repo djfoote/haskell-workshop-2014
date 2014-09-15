f0a7b3824d9481907f694eed80a371fd832625e0
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-09-15 16:32:17 -0700

    Adding bintree example

--- sandbox.hs
+++ sandbox.hs
@@ -12,3 +12,18 @@ bigness x = if x > 50 then "big" else "small"
 
 myMood :: String -> String
 myMood sky = (if sky == "blue" then 'r' else 's') : "ad"
+
+-- Standard Example: Binary Trees
+data BinTree a = Empty
+               | Node a (BinTree a) (BinTree a) deriving (Show)
+
+-- Deriving creates the mentioned functions for the class. In this case, deriving creates the Show (to_string) function for the binary tree node class
+
+-- Function to create a tree from a list:
+-- Creates a tree where first element is the tree node, all elements of list less than first element are on left side, rest are on right side
+treeFromList :: (Ord a) => [a] -> BinTree a
+treeFromList [] = Empty
+treeFromList (x:xs) = Node x (treeFromList (filter (<x) xs))
+                             (treeFromList (filter (>x) xs))
+
+treeMain = print $ treeFromList [7,2,4,8]
